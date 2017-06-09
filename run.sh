59bfee3afe99d9df6192688488fcab1a3b528407
Stephen Kelly <stevekm@users.noreply.github.com>
2017-06-09 11:13:07 -0400

    add project ID naming and change report location reporting

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # set -x
-## USAGE: run.sh "/path/to/demultiplexing_output_dir"
+## USAGE: run.sh "/path/to/demultiplexing_output_dir" ["project_ID"]
 ## EXAMPLE: run.sh "/data/molecpathlab/170602_NB501073_0012_AHCKYCBGX2/Data/Intensities/BaseCalls/Unaligned"
 ## DESCRIPTION: This script will set up and create a report for NextSeq demultiplexing output
 
 
 # ~~~~~ CHECK SCRIPT ARGS ~~~~~ #
-if (( "$#" != "1" )); then
+if (( "$#" < "1" )); then
     echo "ERROR: Wrong number of arguments supplied"
     grep '^##' $0
     exit
@@ -18,7 +18,10 @@ demult_outdir="$1"
 demult_outdir_linkname="Unaligned"
 stats_dir="${demult_outdir}/Stats"
 reports_dir="${demult_outdir}/Reports"
+
+project_ID="${2:-""}"
 demult_stat_file="Demultiplex_Stats.htm"
+demult_report_template="demultiplexing_report.Rmd"
 
 
 # ~~~~~ SET ENVIRONMENT ~~~~~ #
@@ -26,8 +29,15 @@ module unload pandoc
 module load pandoc/1.13.1
 
 compile_report () {
+    local report_template="$1"
+    local project_ID="$2"
     local compile_script="compile_RMD_report.R"
-    local report_template="demultiplexing_report.Rmd"
+
+    if [ "$project_ID" != "" ]; then
+        /bin/cp "${report_template}" "${project_ID}_${report_template}"
+        local report_template="${project_ID}_${report_template}"
+    fi
+
     ./${compile_script} $report_template
 }
 
@@ -59,20 +69,20 @@ fi
 
 # check for existing Demultiplex_Stats.htm, and run
 if [ -f "$demult_stat_file" ]; then
-    compile_report
+    compile_report "$demult_report_template" "$project_ID"
     exit
 elif [ -L "$demult_stat_file" ]; then
-    compile_report
+    compile_report "$demult_report_template" "$project_ID"
     exit
 elif [ -f "${demult_outdir_linkname}/${demult_stat_file}" ]; then
     # ln -fs "${demult_outdir_linkname}/${demult_stat_file}" "${demult_stat_file}"
     /bin/cp "${demult_outdir_linkname}/${demult_stat_file}" "${demult_stat_file}"
-    compile_report
+    compile_report "$demult_report_template" "$project_ID"
     exit
 elif [ ! -f "${demult_outdir_linkname}/${demult_stat_file}" ]; then
     make_demult_html "$reports_dir" "${demult_outdir_linkname}/${demult_stat_file}"
     /bin/cp "${demult_outdir_linkname}/${demult_stat_file}" "${demult_stat_file}"
-    compile_report
+    compile_report "$demult_report_template" "$project_ID"
     exit
 else
     printf "ERROR: Item could not be found: %s\n\n" "$demult_stat_file"
