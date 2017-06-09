0784f9d10a622b09e493380482c2e4c7baf8c516
Stephen Kelly <stevekm@users.noreply.github.com>
2017-06-08 21:11:35 -0400

    fix run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# set -x
 ## USAGE: run.sh "/path/to/demultiplexing_output_dir"
 ## EXAMPLE: run.sh "/data/molecpathlab/170602_NB501073_0012_AHCKYCBGX2/Data/Intensities/BaseCalls/Unaligned"
 ## DESCRIPTION: This script will set up and create a report for NextSeq demultiplexing output
@@ -14,7 +14,7 @@ fi
 
 
 # ~~~~~ GET SCRIPT ARGS ~~~~~ #
-demult_outdir="$1" 
+demult_outdir="$1"
 demult_outdir_linkname="Unaligned"
 stats_dir="${demult_outdir}/Stats"
 reports_dir="${demult_outdir}/Reports"
@@ -28,14 +28,14 @@ module load pandoc/1.13.1
 compile_report () {
     local compile_script="compile_RMD_report.R"
     local report_template="demultiplexing_report.Rmd"
-    $compile_script $report_template
+    ./${compile_script} $report_template
 }
 
 make_demult_html () {
     local reports_dir="$1"
     local demult_stat_file="$2"
     cat ${reports_dir}/html/*/all/all/all/laneBarcode.html | grep -v "href=" > "${demult_stat_file}"
-    
+
 }
 
 
@@ -48,10 +48,10 @@ make_demult_html () {
 [ ! -d "$reports_dir" ] && printf "ERROR: Item does not exists: %s\nExiting...\n\n" "$reports_dir" && exit
 
 # link to the demult outdir
-[ ! -l "$demult_outdir_linkname" ] && ln -s "$demult_outdir" "$demult_outdir_linkname"
+[ ! -L "$demult_outdir_linkname" ] && ln -s "$demult_outdir" "$demult_outdir_linkname"
 
 # make sure it worked...
-if [ ! -l "$demult_outdir_linkname" ]; then
+if [ ! -L "$demult_outdir_linkname" ]; then
     printf "ERROR: Item does not exists: %s\nExiting...\n\n" "$demult_outdir_linkname" && exit
 elif [ ! -d "$demult_outdir_linkname" ]; then
     printf "ERROR: Item does not exists: %s\nExiting...\n\n" "$demult_outdir_linkname" && exit
@@ -61,7 +61,7 @@ fi
 if [ -f "$demult_stat_file" ]; then
     compile_report
     exit
-elif [ -l "$demult_stat_file" ]; then
+elif [ -L "$demult_stat_file" ]; then
     compile_report
     exit
 elif [ -f "${demult_outdir_linkname}/${demult_stat_file}" ]; then
